38b03c7b309490000bfc085366a5f31b007820c3
Connor Li <kjopthegamer@hotmail.com>
2019-12-27 13:33:48 +0930

    Created and Started wall script

---
+++ Simulation/Assets/Scripts/Spawning/Wall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public enum WallPos    
+    {
+        Left,
+        Right,
+        Front,
+        Back
+    }
+
+public class Wall : MonoBehaviour
+{
+
+    public WallPos side;
+
+    Transform transform;
+
+    Transform ground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ground = GameObject.FindGameObjectWithTag("Ground").GetComponent<Transform>();
+
+        switch (side)
+        transform = gameObject.GetComponent<Transform>();
+        {
+            case WallPos.Left:
+                print("hello");
+                break;
+            case WallPos.Right:
+                print("hello");
+                break;
+            case WallPos.Front:
+                print("hello");
+                break;
+            case WallPos.Back:
+                print("hello");
+                break;
+        }
+        transform.localPosition = ground.localPosition;
+    }
+}
